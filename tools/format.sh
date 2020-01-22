322ad1f3382cbac3675c3cf82b950d7aaf882824
kamilritz <kritz@ethz.ch>
2020-01-22 13:41:13 +0100

    format.sh done

--- tools/format.sh
+++ tools/format.sh
@@ -9,14 +9,20 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+if ! hash clang-format-6.0
+then
+    echo -e ${RED}Error: clang-format-6.0 not installed${NC}
+    exit 1
+fi
+
 if [[ $do_format -eq 1 ]]
 then
     # formatting
-    clang-format -i -style=file ${files_to_format}
+    clang-format-6.0 -i -style=file ${files_to_format}
     echo -e ${GREEN}Formatting finished${NC}
 else
     # checking format...
-    clang-format -style=file ${files_to_format} &>/dev/null
+    clang-format-6.0 -style=file  -output-replacements-xml ${files_to_format} | grep -c "<replacement " > /dev/null
     if [[ $? -eq 0 ]]
     then
         echo -e ${RED}Error: need to format${NC}
